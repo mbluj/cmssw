f7f6aa56dec18df01298bee079ff22353163c73b
Michal Bluj <michal.bluj@cern.ch>
2018-10-18 12:14:16 +0200

    Files moved to a new location (dynamically check out in runtime from other repo)

--- RecoTauTag/RecoTau/python/DPFIsolation_cfi.py
+++ RecoTauTag/RecoTau/python/DPFIsolation_cfi.py
@@ -5,6 +5,6 @@ DPFIsolation = cms.EDProducer("DPFIsolation",
     pfcands     = cms.InputTag('packedPFCandidates'),
     taus 	= cms.InputTag('slimmedTaus'),
     vertices    = cms.InputTag('offlineSlimmedPrimaryVertices'),
-    graph_file  = cms.string('RecoTauTag/RecoTau/data/DPFIsolation_2017v0.pb')
+    graph_file  = cms.string('RecoTauTag/TrainingFiles/data/DPFTauId/DPFIsolation_2017v0.pb')
 )
 
--- RecoTauTag/RecoTau/python/DeepTauId_cfi.py
+++ RecoTauTag/RecoTau/python/DeepTauId_cfi.py
@@ -5,6 +5,6 @@ deepTauIdraw = cms.EDProducer("DeepTauId",
     electrons = cms.InputTag('slimmedElectrons'),
     muons = cms.InputTag('slimmedMuons'),
     taus = cms.InputTag('slimmedTaus'),
-    graph_file = cms.string('RecoTauTag/RecoTau/data/deepTau_2017v1_20L1024N.pb')
+    graph_file = cms.string('RecoTauTag/TrainingFiles/data/DeepTauId/deepTau_2017v1_20L1024N.pb')
 )
 
